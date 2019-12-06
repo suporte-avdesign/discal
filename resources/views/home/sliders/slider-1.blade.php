2a3f5deef411b33507c23860dc80e78a73071240
Aanselmo Velame <design@anselmovelame.com.br>
2019-12-05 22:05:19 -0300

    Text Slider Home

--- resources/views/home/sliders/slider-1.blade.php
+++ resources/views/home/sliders/slider-1.blade.php
@@ -3,12 +3,14 @@
         <div class="swiper-wrapper">
             <div class="swiper-slide">
                 <div class="slide-inner half-slide slide-overlay" style="background-image:url({{asset('images/slider/slider1.jpg')}}?{{time()}})">
-                    <div class="home_banner_text">
+                    <div class="home_banner_text_top">
                         <div class="banner-tag">
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-blue">#ClovisAtacado</a>
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-gold">Veja Mais</a>
                         </div>
-                        <h2><a href="javascript:">Anuncie Aqui #01</a></h2>
+                        <h2><a href="javascript:">Quer um boa saída para a crise <br/>Anuncie Conosco!</a></h2>
+                    </div>
+                    <div class="home_banner_text">
                         <div class="author-detail">
                             <p><a href="#"><i class="icon-profile-male"></i> {{getComment('name')}}</a></p>
                             <p><i class="icon-clock"></i> {{getComment('hour')}}</p>
@@ -17,14 +19,12 @@
                     </div>
                 </div>
                 <div class="slide-inner quarter-slide slide-overlay" style="background-image:url({{asset('images/slider/slider2.jpg')}}?{{time()}})">
-                    <div class="home_banner_text">
-                        <!--
+                    <div class="home_banner_text_top">
                         <div class="banner-tag">
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-blue">#ClovisAtacado</a>
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-gold">Veja Mais</a>
                         </div>
-                        <h2><a href="detail.html">Top 10 Dancers you should</a></h2>
-                        -->
+                        <h2><a href="detail.html">Dê o primeiro passo com quem entende do assunto</a></h2>
                         <div class="author-detail">
                             <p><a href="#"><i class="icon-profile-male"></i> {{getComment('name')}}</a></p>
                             <p><i class="icon-clock"></i> {{getComment('day')}}</p>
@@ -51,14 +51,14 @@
             </div>
             <div class="swiper-slide">
                 <div class="slide-inner half-slide slide-overlay" style="background-image:url({{asset('images/slider/slider4.jpg')}}?{{time()}})">
-                    <div class="home_banner_text">
-                        <!--
+                    <div class="home_banner_text_top_left">
                         <div class="banner-tag">
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-blue">#ClovisAtacado</a>
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-gold">Veja Mais</a>
                         </div>
-                        <h2><a href="detail.html">Top 10 Dancers you should</a></h2>
-                        -->
+                        <h2><a href="detail.html">Comece o ano com o Pé Direito!<br>Anuncie com a gente!</a></h2>
+                    </div>
+                    <div class="home_banner_text">
                         <div class="author-detail">
                             <p><a href="#"><i class="icon-profile-male"></i> {{getComment('name')}}</a></p>
                             <p><i class="icon-clock"></i> {{getComment('second')}}</p>
@@ -101,14 +101,19 @@
             </div>
             <div class="swiper-slide">
                 <div class="slide-inner half-slide slide-overlay" style="background-image:url({{asset('images/slider/slider7.jpg')}}?{{time()}})">
-                    <div class="home_banner_text">
-                        <!--
+                    <div class="home_banner_text_top">
                         <div class="banner-tag">
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-blue">#ClovisAtacado</a>
                             <a href="{{route('store', 'clovisatacado')}}" class="tag tag-gold">Veja Mais</a>
                         </div>
-                        <h2><a href="detail.html">Top 10 Dancers you should</a></h2>
-                        -->
+                        <h2><a href="detail.html" style="color: #fff;
+    background: #e36493;
+    padding: 5px 10px;
+    transition: all ease-in-out 0.3s;
+    display: inline-block;
+">Você sabe o que é preciso para dar o pontapé inicial na suas vendas?</a></h2>
+                    </div>
+                    <div class="home_banner_text">
                         <div class="author-detail">
                             <p><a href="#"><i class="icon-profile-male"></i> {{getComment('name')}}</a></p>
                             <p><i class="icon-clock"></i> {{getComment('minute')}}</p>
